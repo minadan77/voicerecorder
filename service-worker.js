8c20fb5a5a1552042dc7d40a88f616ab55a24e15
minadan77 <96655942+minadan77@users.noreply.github.com>
2024-07-16 15:00:29 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'voice-recorder-v2';
+const CACHE_NAME = 'voice-recorder-v3';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/icon.png'
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './icon.png',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -17,6 +18,40 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    function(response) {
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
